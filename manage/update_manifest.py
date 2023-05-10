e06c0ade8967a4d38a2eab9f2836580fe88ae4c3
Sergey <itsib.su@gmail.com>
2023-05-10 23:55:38 +0700

    Fixed manufest scrit

--- manage/update_manifest.py
+++ manage/update_manifest.py
@@ -1,25 +1,23 @@
 """Update the manifest file."""
 import sys
 import json
-import os
 
 
 def update_manifest():
     """Update the manifest file."""
+    manifest_file_url = "custom_components/air_quality/manifest.json"
     version = "0.0.0"
     for index, value in enumerate(sys.argv):
         if value in ["--version", "-V"]:
             version = sys.argv[index + 1]
 
-    with open(f'{os.getcwd()}/custom_components/air_quality/manifest.json') as manifestfile:
-        manifest = json.load(manifestfile)
+    with open(manifest_file_url, 'r') as manifest_file:
+        manifest = json.load(manifest_file)
 
     manifest["version"] = version
 
-    with open(
-        f'{os.getcwd()}/custom_components/air_quality/manifest.json', "w"
-    ) as manifestfile:
-        manifestfile.write(json.dumps(manifest, indent=4, sort_keys=True))
+    with open(manifest_file_url, "w") as manifest_file:
+        manifest_file.write(json.dumps(manifest, indent=4, sort_keys=True))
 
 
 update_manifest()
